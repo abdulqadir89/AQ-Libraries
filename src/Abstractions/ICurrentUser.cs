bbb4194c4a585a64b4616bbce6ba331517e152eb
Abdul Qadir <abdulqadir0@outlook.com>
2026-01-04 20:32:06 +0500

    docs: update ICurrentUserService summary for clarity

--- src/Abstractions/ICurrentUser.cs
+++ src/Abstractions/ICurrentUser.cs
@@ -1,5 +1,9 @@
 ﻿namespace AQ.Abstractions;
 
+/// <summary>
+/// Service to get information about the current authenticated user
+/// Not to be used in endpoints directly
+/// </summary>
 public interface ICurrentUserService
 {
     string? GetCurrentUserId();
